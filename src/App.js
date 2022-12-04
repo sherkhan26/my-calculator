bf9652c5fa35dfb4240464eac9cf1155968c89cc
SherkhanAmangeldyStudy <sherhanamangeldy@gmail.com>
2022-12-04 22:10:14 +0600

    my-calculator-1

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+import './App.css'
+
+const App = () => {
+    const [result, setResult] = useState('')
+
+    const handleClick = (e) => {
+        setResult(result.concat(e.target.name))
+    }
+
+    const clear = () => {
+        setResult('')
+    }
+
+    const backspace = () => {
+        setResult(result.slice(0, result.length - 1))
+    }
+
+    const calculate = () => {
+        setResult(eval(result).toString())
+    }
+    return (
+        <>
+            <div className="container">
+                <form action="">
+                    <input type="text" value={result}/>
+                </form>
+
+                <div className="keypad">
+                    <button onClick={clear} className='clear highlight'>Clear</button>
+                    <button onClick={backspace} className='backspace highlight'>C</button>
+                    <button name='/' onClick={handleClick} className='highlight'>&divide;</button>
+                    <button name='7' onClick={handleClick}>7</button>
+                    <button name='8' onClick={handleClick}>8</button>
+                    <button name='9' onClick={handleClick}>9</button>
+                    <button name='*' onClick={handleClick} className='highlight'>&times;</button>
+                    <button name='4' onClick={handleClick}>4</button>
+                    <button name='5' onClick={handleClick}>5</button>
+                    <button name='6' onClick={handleClick}>6</button>
+                    <button name='-' onClick={handleClick} className='highlight'>&ndash;</button>
+                    <button name='1' onClick={handleClick}>1</button>
+                    <button name='2' onClick={handleClick}>2</button>
+                    <button name='3' onClick={handleClick}>3</button>
+                    <button name='+' onClick={handleClick} className='highlight'>+</button>
+                    <button name='0' onClick={handleClick}>0</button>
+                    <button name='.' onClick={handleClick} className='highlight'>.</button>
+                    <button onClick={calculate} className="result highlight">=</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App
